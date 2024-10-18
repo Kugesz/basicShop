51113ac79c0713299bd065846cc324a2c9d2b625
Kugesz <kovacsgergo.algyo@gmail.com>
2024-10-18 10:25:32 +0200

    fileba mentes

--- model/product.js
+++ model/product.js
@@ -1,4 +1,29 @@
-const products = [];
+import fs from "fs";
+import path from "path";
+
+import __dirname from "../util/rootpath.js";
+import { FILE } from "dns";
+
+const FILE_PATH = path.join(__dirname, "data", "products.json");
+
+const fecthData = () => {
+  let content = [];
+
+  try {
+    content = JSON.parse(fs.readFileSync(FILE_PATH));
+  } catch (err) {
+    console.log(`File reading error ${err}`);
+  }
+  return content;
+};
+
+const setData = (content) => {
+  try {
+    fs.writeFileSync(FILE_PATH, JSON.stringify(content));
+  } catch (err) {
+    console.log(`File writing error ${err}`);
+  }
+};
 
 export class Product {
   constructor(title) {
@@ -6,10 +31,12 @@ export class Product {
   }
 
   save() {
+    const products = fecthData();
     products.push(this);
+    setData(products);
   }
 
   static fetchAll() {
-    return products;
+    return fecthData();
   }
 }
